8755589cafcae43fffade1fb9baa0c4b5e9a9bb1
RoamingHeart <aggoden@gmail.com>
2023-09-07 19:47:43 -0400

    updated photos

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,10 @@ export default function About() {
                 <div className='lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center'>
                     <h1 className='title-font sm:text-4xl text-3xl mb-4 font-medium text-white'>
                         Hello, I Am Franklin
-                        <br className='hidden lg:inline-block' /> I enjoy creating apps.
+                        <br className='hidden lg:inline-block' /> Enjoyer of Creation
                     </h1>
                     <p className='mb-8 leading-relaxed'>
-                        lorem ipsum
+                        Enthusiastic Coder
                     </p>
                     <div className='flex justify-center'>
                         <a 
@@ -21,7 +21,7 @@ export default function About() {
                         <a
                             href="#projects"
                             className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
-                            See My Past Work
+                            My Past Works
                         </a>
                     </div>
                 </div>
@@ -30,7 +30,7 @@ export default function About() {
                         className='object-cover onject-center rounded'
                         alt='person coding'
                         //replace picture later
-                        src='./coding.svg'
+                        src='./obelisk.jpg'
                     />
                 </div>
             </div>
--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const projects = [
     description:
       "A Progressive Web Application that allows for the user to input, delete and edit text. It is an application that could be runned in the browser offline or be installed locally into the user's machine.",
     //replace pic
-    image: "./project-1.gif",
+    image: "./pwa-text.jpg",
     link: "https://github.com/RoamingHeart/pwa-text-editor",
   },
   {
@@ -13,7 +13,7 @@ export const projects = [
     subtitle: "NoSQL Social Network API",
     description:
       "A social network API built with NoSQL using mongoose and express.",
-    image: "./project-2.gif",
+    image: "./socials.jpg",
     link: "https://github.com/RoamingHeart/nosql-socials",
   },
   {
@@ -21,7 +21,7 @@ export const projects = [
     subtitle: "SVG Logo Maker",
     description:
       "An application that creates a logo using user inputs from inquirer prompts and generates a SVG file holding the user's desired image.",
-    image: "./project-3.gif",
+    image: "./svg.jpg",
     link: "https://github.com/RoamingHeart/SVG-logo-maker",
   },
   {
@@ -29,7 +29,7 @@ export const projects = [
     subtitle: "News and Weather Website",
     description:
       "A news and weather website. Allows for the user to see breaking news and what the weather is like at their location.",
-    image: "./project-4.gif",
+    image: "./project2.png",
     link: "https://github.com/RoamingHeart/Project2",
   },
 ];
